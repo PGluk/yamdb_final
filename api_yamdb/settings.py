f31e434e6287271eb267f33c16f8c3602ee04e89
PGluk <glukhov.p@gmail.com>
2021-06-28 19:13:30 +0600

    initial

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -7,13 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "84.201.133.149",
-    "gluk.myddns.me",
-    "www.gluk.myddns.me",
-]
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
